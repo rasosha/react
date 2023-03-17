9d002f7fa4662f0f480a62b81daf185204e21580
Andrei Rassokhin <rasosha@gmail.com>
2023-03-17 19:02:06 +0800

    fix: delete html link, fix eslint config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext',
+  },
   server: {
     port: 12345,
+  },
+  preview: {
+    port: 8080,
   },
 });
