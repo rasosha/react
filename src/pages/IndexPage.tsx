2c16a4c0dec5a6581b3407b50f3c90f3b33da5c4
Andrei Rassokhin <rasosha@gmail.com>
2023-03-19 21:26:04 +0800

    add: pages tests

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,11 +1,11 @@
-import Cards from '../components/Cards';
 import React from 'react';
+import Cards from '../components/Cards';
 import SearchBar from '../components/SearchBar';
 
 function IndexPage() {
   return (
     <>
-      <main className="main">
+      <main className="main" data-testid="IndexPage">
         <SearchBar />
         <Cards />
       </main>
