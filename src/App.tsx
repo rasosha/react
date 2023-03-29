d4436b2c6d02c1f4072cae1f6531f22a3a99661d
Andrei Rassokhin <rasosha@gmail.com>
2023-03-29 16:23:44 +0800

    refactor: rewrite IndexPage with the hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/style.css';
 import IndexPage from './pages/IndexPage';
@@ -7,23 +7,21 @@ import AboutPage from './pages/AboutPage';
 import PageStructure from './components/Page';
 import FormPage from './pages/FormPage';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<PageStructure />}>
-              <Route index element={<IndexPage />} />
-              <Route path="about" element={<AboutPage />} />
-              <Route path="form" element={<FormPage />} />
-              <Route path="*" element={<ErrorPage />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </>
-    );
-  }
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PageStructure />}>
+            <Route index element={<IndexPage />} />
+            <Route path="about" element={<AboutPage />} />
+            <Route path="form" element={<FormPage />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -2,34 +2,33 @@ import React from 'react';
 import Cards from '../components/Cards';
 import SearchBar from '../components/SearchBar';
 
-export class IndexPage extends React.Component {
-  constructor(props: object) {
-    super(props);
-    this.state = {
-      cards: [],
-    };
-  }
-  componentDidMount() {
-    const api =
-      'https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,,17,18,20,21';
-    fetch(api)
-      .then((res) => res.json())
-      .then((data) => {
-        this.setState({
-          cards: data,
+export function IndexPage() {
+  const cardsIds = '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20,21';
+  const api = `https://rickandmortyapi.com/api/character/${cardsIds}`;
+  const [cards, setCards] = React.useState({ cards: [] });
+
+  const getCards = () => {
+    if (cards.cards.length === 0) {
+      fetch(api)
+        .then((res) => res.json())
+        .then((data) => {
+          setCards({
+            cards: data,
+          });
         });
-      });
-  }
-  render() {
-    return (
-      <>
-        <main className="main" data-testid="IndexPage">
-          <SearchBar />
-          <Cards {...this.state} />
-        </main>
-      </>
-    );
-  }
+    }
+  };
+
+  getCards();
+
+  return (
+    <>
+      <main className="main" data-testid="IndexPage">
+        <SearchBar />
+        <Cards {...cards} />
+      </main>
+    </>
+  );
 }
 
 export default IndexPage;
