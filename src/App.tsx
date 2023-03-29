c55c0c652149ca47c5b5f9ea626304f29a7547e7
Andrei Rassokhin <rasosha@gmail.com>
2023-03-29 17:41:01 +0800

    Revert "refactor: rewrite IndexPage with the hooks"
    This reverts commit d4436b2c6d02c1f4072cae1f6531f22a3a99661d.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/style.css';
 import IndexPage from './pages/IndexPage';
@@ -7,21 +7,23 @@ import AboutPage from './pages/AboutPage';
 import PageStructure from './components/Page';
 import FormPage from './pages/FormPage';
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<PageStructure />}>
-            <Route index element={<IndexPage />} />
-            <Route path="about" element={<AboutPage />} />
-            <Route path="form" element={<FormPage />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<PageStructure />}>
+              <Route index element={<IndexPage />} />
+              <Route path="about" element={<AboutPage />} />
+              <Route path="form" element={<FormPage />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -2,32 +2,34 @@ import React from 'react';
 import Cards from '../components/Cards';
 import SearchBar from '../components/SearchBar';
 
-export function IndexPage() {
-  document.title = `Main Page`;
-  const cardsIds = '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20,21';
-  const api = `https://rickandmortyapi.com/api/character/${cardsIds}`;
-  const [cards, setCards] = React.useState({ cards: [] });
-
-  (() => {
-    if (cards.cards.length === 0) {
-      fetch(api)
-        .then((res) => res.json())
-        .then((data) => {
-          setCards({
-            cards: data,
-          });
+export class IndexPage extends React.Component {
+  constructor(props: object) {
+    super(props);
+    this.state = {
+      cards: [],
+    };
+  }
+  componentDidMount() {
+    const api =
+      'https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,,17,18,20,21';
+    fetch(api)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          cards: data,
         });
-    }
-  })();
-
-  return (
-    <>
-      <main className="main" data-testid="IndexPage">
-        <SearchBar />
-        <Cards {...cards} />
-      </main>
-    </>
-  );
+      });
+  }
+  render() {
+    return (
+      <>
+        <main className="main" data-testid="IndexPage">
+          <SearchBar />
+          <Cards {...this.state} />
+        </main>
+      </>
+    );
+  }
 }
 
 export default IndexPage;
